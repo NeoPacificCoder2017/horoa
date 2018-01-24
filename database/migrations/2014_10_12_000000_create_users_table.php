2b2f472372f4d47ca4cf1299f7983c43c4976366
VaiariiTepa <tepavaiarii@gmail.com>
2018-01-23 16:03:48 -1000

    migration "USERS" terminer

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('nom');
+            $table->string('prenom');
             $table->string('email')->unique();
             $table->string('password');
-            $table->rememberToken();
+            $table->integer('profile_type_id');
             $table->timestamps();
         });
     }
