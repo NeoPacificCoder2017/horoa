82cd96385434d86bdc871e3798fe836a65056075
VaiariiTepa <tepavaiarii@gmail.com>
2018-01-25 09:18:22 -1000

    Route Mode de Don creer

--- routes/web.php
+++ routes/web.php
@@ -15,21 +15,29 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+/*
+**ROUTE Entity_USERS
+*/
 Route::get('users','UserController@all');
-
-
 Route::get('/users/new',function (){
-
     return view('user-form');
-
 });
-
 Route::post('users/new','UserController@new');
-Route::post('{userID}','UserController@update');
-
 Route::get('users/{userId}','UserController@show');
-
 Route::get('users/{userId}/edit','UserController@edit');
+Route::post('{userId}','UserController@update');
+
+/*
+**ROUTE Entity_ModeDon
+*/
+Route::get('modedons','ModeDonController@all');
+Route::get('/modedon/new', function(){
+    return view('modedon-form');
+});
+Route::post('modedon/new','ModeDonController@new');
+Route::get('modedon/{modedonId}','ModeDonController@show');
+Route::get('modedon/{modedonId}','ModeDonController@edit');
+Route::post('{modedonId}','ModeDonController@update');
 
 
 
