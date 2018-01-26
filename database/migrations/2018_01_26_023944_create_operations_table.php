dfd0e6f4c290f6f6634012dd684f152ce9be0e01
Lysbro <eleve5@cnampf.com>
2018-01-25 16:44:53 -1000

    migration done

---
+++ database/migrations/2018_01_26_023944_create_operations_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOperationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('operations', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('nom');
+            $table->date('date_debut');
+            $table->date('date_fin');
+            $table->integer('createur_id');
+            $table->integer('total_recolter');
+            $table->integer('total_promis');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('operations');
+    }
+}
