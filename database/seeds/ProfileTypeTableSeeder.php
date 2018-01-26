3ba0df7d9a35c0d40e13c0f70e08fec702a78344
Lysbro <eleve5@cnampf.com>
2018-01-26 00:26:07 -1000

    update

---
+++ database/seeds/ProfileTypeTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\ProfileType;
+
+class ProfileTypeTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+         $profilesTypes = [ ];
+        
+        $nom = ['super_admin','admin','donnateur','createur'];
+
+        for($i = 1; $i < 4; $i++) :
+            $profilesTypes [ ]  = [
+                'nom' => $nom[$i]
+            ];
+        endfor;
+        
+        foreach($profilesTypes as $profileType):
+
+            ProfileType::create($profileType);
+            
+        endforeach;
+    }
+}
